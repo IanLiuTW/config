d20478b7fab91de1413c72be8c2019f8870bf92c
Ian Liu <ian.liu@tupl.com>
2024-07-29 08:30:14 -0700

    Update nvim

---
+++ nvim/.config/nvim/lua/plugins/task/task.lua
@@ -0,0 +1,77 @@
+return {
+  {
+    'stevearc/overseer.nvim',
+    cmd = {
+      'OverseerOpen',
+      'OverseerClose',
+      'OverseerToggle',
+      'OverseerSaveBundle',
+      'OverseerLoadBundle',
+      'OverseerDeleteBundle',
+      'OverseerRunCmd',
+      'OverseerRun',
+      'OverseerInfo',
+      'OverseerBuild',
+      'OverseerQuickAction',
+      'OverseerTaskAction',
+      'OverseerClearCache',
+    },
+    keys = {
+      { '<leader>oo', '<cmd>OverseerToggle<cr>', desc = 'Task list' },
+      { '<leader>or', '<cmd>OverseerRun<cr>', desc = 'Run task' },
+      { '<leader>oq', '<cmd>OverseerQuickAction<cr>', desc = 'Action recent task' },
+      { '<leader>oi', '<cmd>OverseerInfo<cr>', desc = 'Overseer Info' },
+      { '<leader>ob', '<cmd>OverseerBuild<cr>', desc = 'Task builder' },
+      { '<leader>ot', '<cmd>OverseerTaskAction<cr>', desc = 'Task action' },
+      { '<leader>oc', '<cmd>OverseerClearCache<cr>', desc = 'Clear cache' },
+      { '<leader>os', '<cmd>OverseerSaveBundle<cr>', desc = 'Save Bundle' },
+      { '<leader>ol', '<cmd>OverseerLoadBundle<cr>', desc = 'Load Bundle' },
+      { '<leader>od', '<cmd>OverseerDeleteBundle<cr>', desc = 'Delete Bundle' },
+    },
+    config = function()
+      require('overseer').setup {
+        strategy = 'toggleterm',
+        templates = { 'builtin' },
+      }
+    end,
+    init = function()
+      vim.api.nvim_create_user_command('OverseerRestartLast', function()
+        local overseer = require 'overseer'
+        local tasks = overseer.list_tasks { recent_first = true }
+        if vim.tbl_isempty(tasks) then
+          vim.notify('No tasks found', vim.log.levels.WARN)
+        else
+          overseer.run_action(tasks[1], 'restart')
+        end
+      end, {})
+      vim.keymap.set('n', '<leader>o.', '<cmd>OverseerRestartLast<cr>', { noremap = true, silent = true, desc = 'Restart last task' })
+      vim.keymap.set('n', '<leader>oz', function() --Run shell scripts in the current directory
+        local files = require 'overseer.files'
+        return {
+          generator = function(opts, cb)
+            local scripts = vim.tbl_filter(function(filename)
+              return filename:match '%.sh$'
+            end, files.list_files(opts.dir))
+            local ret = {}
+            for _, filename in ipairs(scripts) do
+              table.insert(ret, {
+                name = filename,
+                params = {
+                  args = { optional = true, type = 'list', delimiter = ' ' },
+                },
+                builder = function(params)
+                  return {
+                    cmd = { files.join(opts.dir, filename) },
+                    args = params.args,
+                  }
+                end,
+              })
+            end
+
+            cb(ret)
+          end,
+        }
+      end, { noremap = true, silent = true, desc = 'Add shell scripts in cwd' })
+    end,
+  },
+}
