a60c3620bacb58a5a507117ebb4f010f2a139370
Ian Liu <ian.liu@tupl.com>
2024-08-01 15:38:03 -0700

    Update nvim

---
+++ nvim/.config/nvim/lua/plugins/ui/noice.lua
@@ -0,0 +1,64 @@
+return {
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    'MunifTanjim/nui.nvim',
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    'rcarriga/nvim-notify',
+  },
+  config = function()
+    require('noice').setup {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = false, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+      routes = {
+        { view = 'cmdline', filter = { event = 'msg_showmode' } },
+        -- { view = 'notify', filter = { event = 'msg_showmode' } },
+        -- { view = 'cmdline', filter = { event = 'msg_show', kind = '', find = 'written' } },
+        { filter = { event = 'msg_show', kind = '', find = 'written' }, opts = { skip = true } },
+        {
+          filter = {
+            event = 'lsp',
+            kind = 'progress',
+            cond = function(message)
+              local client = vim.tbl_get(message.opts, 'progress', 'client')
+              return client == 'lua_ls'
+            end,
+          },
+          opts = { skip = true },
+        },
+      },
+      views = {
+        cmdline_popup = {
+          border = {
+            style = 'double',
+            padding = { 0, 0 },
+          },
+          filter_options = {},
+          win_options = {
+            winhighlight = 'NormalFloat:NormalFloat,FloatBorder:FloatBorder',
+          },
+        },
+      },
+    }
+  end,
+}
