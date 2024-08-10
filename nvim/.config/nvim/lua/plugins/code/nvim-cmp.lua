514551bd60c4e64408cde083e3ca957be60634d1
Ian Liu <ian.liu@tupl.com>
2024-08-10 12:17:46 -0700

    update: nvim

--- nvim/.config/nvim/lua/plugins/code/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/code/nvim-cmp.lua
@@ -36,6 +36,7 @@ return { -- Autocompletion
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-cmdline',
   },
   config = function()
     -- See `:help cmp`
@@ -151,5 +152,29 @@ return { -- Autocompletion
         { name = 'path' },
       },
     }
+
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' },
+      },
+    })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' },
+      }, {
+        {
+          name = 'cmdline',
+          option = {
+            ignore_cmds = { 'Man', '!' },
+          },
+        },
+      }),
+      matching = { disallow_symbol_nonprefix_matching = false },
+    })
   end,
 }
