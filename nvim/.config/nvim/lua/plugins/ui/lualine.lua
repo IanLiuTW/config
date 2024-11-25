1f8599a49122041759e336c777fad8869e3b8c25
Ian Liu <ian.liu@tupl.com>
2024-11-24 23:41:21 -0800

    update: nvim

--- nvim/.config/nvim/lua/plugins/ui/lualine.lua
+++ nvim/.config/nvim/lua/plugins/ui/lualine.lua
@@ -27,6 +27,18 @@ return {
           { 'filename', file_status = true, path = 1 },
         },
         lualine_x = {
+          {
+            function()
+              local isVisualMode = vim.fn.mode():find '[Vv]'
+              if not isVisualMode then
+                return ''
+              end
+              local starts = vim.fn.line 'v'
+              local ends = vim.fn.line '.'
+              local lines = starts <= ends and ends - starts + 1 or starts - ends + 1
+              return '󰒇 ' .. tostring(lines) .. 'L ' .. tostring(vim.fn.wordcount().visual_chars) .. 'C'
+            end,
+          },
           { 'fancy_macro' },
           { 'fancy_searchcount' },
           { 'fancy_location' },
