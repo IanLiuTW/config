01295efa0eb7b0524fd1fd754a929d8299db4df7
Ian Liu <ian.liu@tupl.com>
2025-02-15 09:57:01 -0800

    update: nvim

--- nvim/.config/nvim/lua/plugins/language/nvim-conform.lua
+++ nvim/.config/nvim/lua/plugins/language/nvim-conform.lua
@@ -6,7 +6,7 @@ return {
     {
       '<leader>f',
       function()
-        require('conform').format()
+        require('conform').format { async = true, lsp_fallback = true }
       end,
       mode = 'n',
       desc = 'Buffer - Format (Conform)',
