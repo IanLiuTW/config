d276d636003a7f1eb5d9c0534f0908f9b33303ff
Ian Liu <ian.liu@tupl.com>
2025-07-03 23:59:48 -0700

    update: nvim

--- nvim/.config/nvim/lua/plugins/git/git-conflict.lua
+++ nvim/.config/nvim/lua/plugins/git/git-conflict.lua
@@ -11,5 +11,10 @@ return {
       next = 'n',
       prev = 'p',
     },
+    disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
+    highlights = { -- They must have background color, otherwise the default color will be used
+      incoming = 'DiffAdd',
+      current = 'DiffText',
+    },
   },
 }
